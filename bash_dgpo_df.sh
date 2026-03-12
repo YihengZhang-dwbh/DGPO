56a4e01c948d488f0e205a901eb87d33385d15bd
YihengZhang-dwbh <1229914511@qq.com>
2026-03-12 17:58:25 +0800

    Changes to be committed: 	new file:   bash_dgpo_df.sh 	new file:   bash_dgpo_fm.sh

---
+++ bash_dgpo_df.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+source activate DGPO
+
+id=0
+env_name="HalfCheetah-v4"
+seed=1
+time=$(date '+%Y-%m-%d-%H%M%S')
+
+mkdir -p logs
+
+echo "🚀 [$(date '+%H:%M:%S')] is ready to run DGPO in ${env_name} on GPU ${id}"
+
+echo "Run DGPO (Diffusion)... > logs/train_dgpo_diffusion_${env_name}_${time}.txt"
+CUDA_VISIBLE_DEVICES=$id python scripts/train_dgpo_diffusion.py \
+    --env-name $env_name \
+    --seed $seed \
+    > logs/train_dgpo_diffusion_${env_name}_${time}.txt 2>&1 &
---
+++ bash_dgpo_fm.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+source activate DGPO
+
+id=0
+env_name="HalfCheetah-v4"
+seed=1
+time=$(date '+%Y-%m-%d-%H%M%S')
+
+mkdir -p logs
+
+echo "🚀 [$(date '+%H:%M:%S')] is ready to run DGPO in ${env_name} on GPU ${id}"
+
+echo "Run DGPO (Flow Matching)... > logs/train_dgpo_fm_${env_name}_${time}.txt"
+CUDA_VISIBLE_DEVICES=$id python scripts/train_dgpo_fm.py \
+    --env-name $env_name \
+    --seed $seed \
+    > logs/train_dgpo_fm_${env_name}_${time}.txt 2>&1 &
