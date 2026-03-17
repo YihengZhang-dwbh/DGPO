07e57a75bfbb2189326c772acea6393180ab5630
YihengZhang-dwbh <1229914511@qq.com>
2026-03-17 19:18:40 +0800

    惊天一条蛆，给我把对的顺序改错了，根本没问题

--- src/flow_policy/dgpo_fm.py
+++ src/flow_policy/dgpo_fm.py
@@ -308,7 +308,7 @@ class DGPOFMState:
                                 opt_state_policy=new_p_opt, opt_state_value=new_v_opt_state, log_cql_weight=new_la,
                                 steps=self.steps + 1
                                 )
-        
+
         # 在 _step_minibatch 的最后一行，把 fresh_metrics 塞进去：
         final_metrics = {**{k: v[-1] for k, v in extra_v_metrics.items()}, **p_metrics, **fresh_metrics}
         return new_state, final_metrics
@@ -396,7 +396,7 @@ class DGPOFMState:
         bootstrap_q, _ = networks.value_mlp_fwd_with_features(self.params.value, bootstrap_concat)
         bootstrap_q = jax.lax.stop_gradient(bootstrap_q)
 
-        _, target_qs = jax.lax.stop_gradient(
+        target_qs, _ = jax.lax.stop_gradient(
             rollouts.compute_gae(
                 truncation=transitions.truncation,
                 discount=transitions.discount * self.config.discounting,
@@ -645,7 +645,7 @@ class DGPOFMState:
                                                               jnp.concatenate([bootstrap_obs, bootstrap_act], axis=-1))
 
         # 3. 计算绝对静态的 GAE Target
-        _, target_qs = jax.lax.stop_gradient(
+        target_qs, _ = jax.lax.stop_gradient(
             rollouts.compute_gae(
                 truncation=transitions.truncation,
                 discount=transitions.discount * config.discounting,
